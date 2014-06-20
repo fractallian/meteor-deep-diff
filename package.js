6e14d9e9303194782425e6104ebdc710ee3b306b
Jeremy <fractallian@gmail.com>
2014-06-20 16:31:46 -0700

    meteor repackage

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: 'deep-diff repackaged for meteor'
+});
+
+Package.on_use(function (api, where) {
+  api.use(['underscore'], ['client', 'server']);
+  api.add_files(['lib/diff/index.js'], ['client', 'server']);
+
+  if (typeof api.export !== 'undefined') {
+    // api.export('Model', ['client', 'server']);
+    api.export('DeepDiff', ['client', 'server']);
+  }
+});
+
+Package.on_test(function (api) {
+  api.use(['graviton', 'tinytest', 'test-helpers']);
+
+  api.add_files('lib/diff/test/tests.js', ['client', 'server']);
+});
+
+
